928b181460d533c05ffe2e39d9eea7a14a46cd7c
icewarbler <beaumagro@gmail.com>
2025-08-08 17:51:19 -0500

    Cleans up shell_main

--- shell_main.c
+++ shell_main.c
@@ -1,11 +1,5 @@
-/**
- * shell
- * CS 341 - Fall 2024
- */
 #include "shell.h"
 
-// Seperate main file used for grading. Do not modify!
 int main(int argc, char **argv) {
-    // calls the student code
     shell(argc, argv);
 }
